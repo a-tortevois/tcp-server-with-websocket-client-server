53974ebeabf24df877ef311a1dd88fd93ca2de71
Alexandre <a.tortevois@arfitec.com>
2020-02-23 16:04:41 +0100

    Refactor the server thread exit

--- nonblocking_server.c
+++ nonblocking_server.c
@@ -13,6 +13,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <netinet/in.h>
+#include <sys/ioctl.h>
 #include <sys/socket.h>
 
 #define MIN(a, b) (((a)<(b))?(a):(b))
@@ -21,23 +22,34 @@
 #define SERVER_SOCKET_PORT 1998
 #define BUFFER_MAX_SIZE 16*1024 // 16ko
 
-static bool isRunning = true;
+//static bool isRunning = true;
+volatile sig_atomic_t isRunning = true;
 static pthread_t thread_id = -1;
+static int server_socket = -1, client_socket = -1;
 static char reply_buffer[BUFFER_MAX_SIZE];
 
-static int server_create(int *server_socket) {
+static int server_create(void) {
     int rc = 0, on = 1;
     struct sockaddr_in server_addr;
 
     // Create an endpoint for communication
-    if ((*server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Unable to open `server` socket");
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Unable to open the server socket");
         rc = -1;
         goto __error;
     }
 
     // Allow address to be reused instantly
-    setsockopt(*server_socket, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+
+    // Set socket to be nonblocking
+    /*
+    if (ioctl(*server_socket, FIONBIO, (char *) &on) < 0) {
+        perror("Unable to set `socket` to be nonblocking");
+        rc = -1;
+        goto __error;
+    }
+    */
 
     // Assign IP, PORT
     server_addr.sin_family = AF_INET;
@@ -45,19 +57,19 @@ static int server_create(int *server_socket) {
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     // Bind the newly created socket
-    if (bind(*server_socket, (const struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
-        perror("Unable to bind `server` socket");
+    if (bind(server_socket, (const struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+        perror("Unable to bind the server socket");
         rc = -1;
         goto __error;
     }
 
-    printf("Thread `server` is running on port %d\n", SERVER_SOCKET_PORT);
+    printf("Server thread is running on port %d\n", SERVER_SOCKET_PORT);
 
     __error:
     return rc;
 }
 
-static int write_to_socket(int socket) {
+static int write_to_client(void) {
     int rc = 0;
     // Build a response
     char time_str[10];
@@ -68,9 +80,8 @@ static int write_to_socket(int socket) {
     // strcpy(reply_buffer, "{}");
     sprintf(reply_buffer, "It's %s\n", time_str);
 
-    int msg_len = strlen(reply_buffer);
-
-    if (write(socket, reply_buffer, strlen(reply_buffer)) != msg_len) {
+    unsigned int msg_len = strlen(reply_buffer);
+    if (write(client_socket, reply_buffer, msg_len) != msg_len) {
         printf("Error write #%d: %s\n", errno, strerror(errno));
         if (errno == EPIPE) {
             printf("Catch error EPIPE\n");
@@ -81,22 +92,31 @@ static int write_to_socket(int socket) {
         //exit(EXIT_FAILURE);
     }
 
-    printf("Message send to the client: %s\n", reply_buffer);
+    printf("Message send to the client: %s", reply_buffer);
     __error:
     return rc;
 }
 
 static void *server_thread(void __attribute__((unused)) *p) {
-    int server_socket = -1, client_socket = -1;
     fd_set set;
     int max_fd_set;
     int activity;
     struct timeval timeout;
 
+    // Allow the thread to be canceled
+    pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+    pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+
     // Disable SIGPIPE
     signal(SIGPIPE, SIG_IGN);
 
-    if (server_create(&server_socket) < 0) {
+    if (server_create() < 0) {
+        goto __error;
+    }
+
+    // Listen for connections on a socket
+    if (listen(server_socket, 1) < 0) { // only one client
+        perror("Unable to listen the server socket");
         goto __error;
     }
 
@@ -104,13 +124,7 @@ static void *server_thread(void __attribute__((unused)) *p) {
 
         printf("Waiting for a new client\n");
 
-        // Listen for connections on a socket
-        if (listen(server_socket, 1) < 0) { // only one client
-            perror("Error listen");
-            goto __error;
-        }
-
-        // Accept a connection on a socket : wait a new client
+        // Accept a connection on a socket
         if ((client_socket = accept(server_socket, NULL, NULL)) < 0) {
             perror("Unable to accept a client");
             goto __error;
@@ -125,22 +139,21 @@ static void *server_thread(void __attribute__((unused)) *p) {
 
         while (isRunning) {
             // Initialize timeout
-            timeout.tv_sec = 20; // 10 sec
+            timeout.tv_sec = 30; // 10 sec
             timeout.tv_usec = 0;
 
             activity = select(max_fd_set, &set, NULL, NULL, &timeout);
 
             // Error
             if (activity < 0) {
-                perror("select() failed");
-                exit(EXIT_FAILURE);
+                perror("Failed to select");
+                goto __error; // exit(EXIT_FAILURE);
             }
 
             // Timeout
             if (activity == 0) {
                 // If client is disconnected => break; else continue
-                if (write_to_socket(client_socket) < 0) break;
-
+                if (write_to_client() < 0) break;
                 continue;
             }
 
@@ -156,7 +169,7 @@ static void *server_thread(void __attribute__((unused)) *p) {
             }
 
             // Client is disconnected
-            if (msg_len == 0) break; // TODO with SIGPIPE ?
+            if (msg_len == 0) break;
 
             // Check the minimal length
             if (msg_len < 2) {
@@ -169,18 +182,28 @@ static void *server_thread(void __attribute__((unused)) *p) {
             printf("Message received from the client: %s\n", buffer);
 
             // If client is disconnected => break;
-            if (write_to_socket(client_socket) < 0) break;
+            if (write_to_client() < 0) break;
         }
 
         // Client is disconnected
         printf("Client is now disconnected\n");
         close(client_socket);
+        client_socket = -1;
     }
 
     __error:
+    if (client_socket != -1) {
+        close(client_socket);
+        client_socket = -1;
+    }
+
     if (server_socket != -1) {
         close(server_socket);
+        server_socket = -1;
     }
+
+    printf("End of thread `server`");
+    exit(EXIT_FAILURE); // pthread_exit(0);
 }
 
 static int server_start(void) {
@@ -196,11 +219,24 @@ static int server_start(void) {
 }
 
 static void server_stop(void) {
+    printf("server_stop()\n");
     isRunning = false;
     if (thread_id != -1) {
-        // pthread_join(thread_id, NULL);
-        printf("Thread `server` stop");
+        pthread_cancel(thread_id);
+        if (client_socket != -1) {
+            if(shutdown(client_socket, SHUT_RDWR) < 0) {
+                printf("Unable to close the client socket: %s\n", strerror(errno));
+            }
+        }
+        if (server_socket != -1) {
+            if(shutdown(server_socket, SHUT_RDWR) < 0) {
+                printf("Unable to close the server socket: %s\n", strerror(errno));
+            }
+
+        }
+        pthread_join(thread_id, NULL);
     }
+    printf("Thread `server` stop\n");
 }
 
 static void sig_handler(int sig) {
@@ -251,15 +287,9 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    //    const char *command = "node /root/websocket_server/wss.js &";
-    //    if (system(command) < 0) {
-    //        perror("Unable to launch the command");
-    //        exit(EXIT_FAILURE);
-    //    }
-
     while (1) {
         usleep(500000);
     }
 
-    return EXIT_FAILURE;
+    exit(EXIT_FAILURE);
 }
